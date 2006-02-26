e4c6abbdedbeedc6ba1ac949b8848de03f0d087f
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-02-26 22:55:04 +0000

    adapt to changes in CondCore

--- test/testSiStripPedestals.cpp
+++ test/testSiStripPedestals.cpp
@@ -1,29 +1,15 @@
-#include "Reflex/Type.h"
-#include "PluginManager/PluginManager.h"
-#include "POOLCore/Token.h"
-#include "FileCatalog/URIParser.h"
-#include "FileCatalog/FCSystemTools.h"
-#include "FileCatalog/IFileCatalog.h"
-#include "StorageSvc/DbType.h"
-#include "PersistencySvc/DatabaseConnectionPolicy.h"
-#include "PersistencySvc/ISession.h"
-#include "PersistencySvc/ITransaction.h"
-#include "PersistencySvc/IDatabase.h"
-#include "PersistencySvc/Placement.h"
-#include "DataSvc/DataSvcFactory.h"
-#include "DataSvc/IDataSvc.h"
-#include "DataSvc/ICacheSvc.h"
-#include "DataSvc/Ref.h"
-
-//#include "SealUtil/SealTimer.h"
-//#include "SealUtil/TimingReport.h"
-//#include "SealUtil/SealHRRTChrono.h"
-//#include "SealUtil/SealHRChrono.h"
+#include "CondCore/DBCommon/interface/DBWriter.h"
+#include "CondCore/DBCommon/interface/DBSession.h"
+#include "CondCore/DBCommon/interface/Exception.h"
+#include "CondCore/DBCommon/interface/ServiceLoader.h"
+#include "CondCore/DBCommon/interface/ConnectMode.h"
+#include "CondCore/DBCommon/interface/MessageLevel.h"
+#include "CondCore/IOVService/interface/IOV.h"
+#include "CondCore/MetaDataService/interface/MetaData.h"
+#include "FWCore/Framework/interface/IOVSyncValue.h"
 
 #include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
 
-#include "CondCore/IOVService/interface/IOV.h"
-#include "CondCore/MetaDataService/interface/MetaData.h"
 #include "CLHEP/Random/RandFlat.h"
 #include "CLHEP/Random/RandGauss.h"
 #include<iostream>
@@ -31,8 +17,7 @@
 
 #include<boost/cstdint.hpp>
 
-#include "POOLCore/POOLContext.h"
-#include "SealKernel/Exception.h"
+
 
 static const  int FedId_Mask_   = 0x3FF;
 static const  int feFPGA_Mask_  = 0x7;
@@ -64,180 +49,160 @@ uint32_t EncodeStripData(float ped_, float noise_, float lowTh_, float highTh_, 
 
 
 int main(int csize, char** cline ) {
-  const std::string userNameEnv = "POOL_AUTH_USER=cms_xiezhen_dev";
-  ::putenv( const_cast<char*>( userNameEnv.c_str() ) );
-  const std::string passwdEnv = "POOL_AUTH_PASSWORD=xiezhen123";
-  ::putenv( const_cast<char*>( passwdEnv.c_str() ) );
-  //std::string m_dbConnection( "oracle://devdb10/cms_xiezhen_dev");
-  std::string m_dbConnection( "sqlite_file:test_SiStripPedestals.db");
-  std::cout<<"connecting..."<<m_dbConnection<<std::endl;
-  seal::PluginManager::get()->initialise();
-  //seal::TimingReport timRep;
+  cond::ServiceLoader* loader=new cond::ServiceLoader;
+  ::putenv("CORAL_AUTH_USER=me");
+  ::putenv("CORAL_AUTH_PASSWORD=mypass");
+  loader->loadAuthenticationService( cond::Env );
+  loader->loadMessageService( cond::Error );
 
+  std::string dbConnection( "sqlite_file:test_SiStripPedestals.db");
+  std::string dbCatalog("file:test_catalog.xml");
 
-
+  std::cout<<"DB connection to "<<dbConnection<<std::endl;
+  std::cout<<"catalog..."<<dbCatalog<<std::endl;
   
-  try {
-    // Loads the seal message stream
-    pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
-    pool::POOLContext::loadComponent( "POOL/Services/EnvironmentAuthenticationService" );
+  try{
+    cond::DBSession* session=new cond::DBSession(dbConnection);
+    session->setCatalog(dbCatalog);
 
+    cond::MetaData metadata_svc(dbConnection,*loader);
+    metadata_svc.connect();
 
-    pool::URIParser p;
-    p.parse();
-    
-    pool::IFileCatalog lcat;
-    pool::IFileCatalog * cat = &lcat;
-    cat->setWriteCatalog(p.contactstring());
-    cat->connect();
-    cat->start();
-    
-    pool::IDataSvc *svc = pool::DataSvcFactory::instance(cat);
-    // Define the policy for the implicit file handling
-    pool::DatabaseConnectionPolicy policy;
-    policy.setWriteModeForNonExisting(pool::DatabaseConnectionPolicy::CREATE);
-    // policy.setWriteModeForExisting(pool::DatabaseConnectionPolicy::OVERWRITE);
-    policy.setWriteModeForExisting(pool::DatabaseConnectionPolicy::UPDATE);
-    svc->session().setDefaultConnectionPolicy(policy);
-    cond::MetaData meta(m_dbConnection);
+    cond::DBWriter pwriter(*session, "SiStripPedestals");
+    cond::DBWriter iovwriter(*session, "IOV");
+    cond::IOV* pedIov=new cond::IOV;
 
+    if (csize==1){ //write      
+      std::cout << "Writing mode" << std::endl;
+      session->connect(cond::ReadWriteCreate);
+      std::cout<<"connected in ReadWriteCreate mode to "<<dbConnection<<std::endl;
+      session->startUpdateTransaction();
 
-    if (csize==1) { //write
-      long int tech(pool::POOL_RDBMS_StorageType.type()); 
-      svc->transaction().start(pool::ITransaction::UPDATE);
-      pool::Ref<cond::IOV> pedIov(svc, new cond::IOV);
-      pool::Placement iovPlace(m_dbConnection, pool::DatabaseSpecification::PFN, "IOV", ROOT::Reflex::Type(), tech); 
-      pedIov.markWrite(iovPlace);
-      std::string iovtoken=pedIov.toString();
-      
-      std::cout << "iovtoken " << iovtoken << std::endl;
-
-      pool::Placement pedPlace(m_dbConnection, pool::DatabaseSpecification::PFN, "Pedestals", ROOT::Reflex::Type(), tech); 
-      
       //int totmodules=16000;
       int iovn=0;
       int evtn=10;
-      for (int j=0; j<evtn; ++j) {
+      for (int j=0; j<evtn; ++j){
 	iovn=j%5;//till time
 	if(iovn==0){
-	  pool::Ref<SiStripPedestals> ped(svc,new SiStripPedestals);
-	  ped.markWrite(pedPlace);
-
-	  std::string pedtoken=ped.toString();
-	  std::cout << "pedtoken " << pedtoken << std::endl;
-
-	  pedIov->iov[j+5]=pedtoken;
+	  SiStripPedestals* ped=new SiStripPedestals();
 	  for(int fedid=1; fedid<=FedId_Max; ++fedid)
 	    for(int fefpga=1; fefpga<=feFPGA_Max; ++fefpga)
 	      for(int ch=1; ch<=channel_Max; ++ch)
-		for(int apvpair=1;apvpair<=ApvPair_Max;apvpair++)
-		  {	   
-		    unsigned int numschema = EncodeApvIdValues(fedid,fefpga,ch,apvpair);
-		    std::cout << "numschema " << numschema <<  std::endl;
-
-		    SiStripPedestalsVector theSiStripVector;
- 		    for(int strip=0; strip<128; ++strip){
-		      SiStripPedestals::SiStripData theSiStripData;
-		      
-		      std::cout << " strip " << strip << " =\t";
-		      theSiStripData.Data = (*ped).EncodeStripData(
-								   RandGauss::shoot(MeanPed,RmsPed),
-								   RandGauss::shoot(MeanNoise,RmsNoise),
-								   2,
-								   5,
-								   (RandFlat::shoot(1.) < badStripProb ? true:false),
-								   1);
-		     
-		      theSiStripVector.push_back(theSiStripData);
-		    }
+		for(int apvpair=1;apvpair<=ApvPair_Max;apvpair++){	   
+		  unsigned int numschema = EncodeApvIdValues(fedid,fefpga,ch,apvpair);
+		  //std::cout << "numschema " << numschema <<  std::endl;
+		  
+		  SiStripPedestalsVector theSiStripVector;
+		  for(int strip=0; strip<128; ++strip){
+		    SiStripPedestals::SiStripData theSiStripData;
 		    
-		    ped->m_pedestals.insert(std::pair<unsigned int, SiStripPedestalsVector > (numschema,theSiStripVector));
-		  }	
+		    bool printstrip= (strip==-1) ? true : false;
+		  
+		    if (printstrip)
+		      std::cout << " strip " << strip << " =\t";
+		    theSiStripData.Data = ped->EncodeStripData(
+							       RandGauss::shoot(MeanPed,RmsPed),
+							       RandGauss::shoot(MeanNoise,RmsNoise),
+							       2,
+							       5,
+							       (RandFlat::shoot(1.) < badStripProb ? true:false),
+							       printstrip);
+		    
+		    theSiStripVector.push_back(theSiStripData);
+		  }
+		  ped->m_pedestals.insert(std::pair<unsigned int, SiStripPedestalsVector > (numschema,theSiStripVector));
+		}
+	  std::string pedtoken=pwriter.markWrite<SiStripPedestals>(ped);
+	  unsigned long iov_idx=j+5;
+	  std::cout << "iov idx " << iov_idx << " pedtoken " << pedtoken << std::endl; 
+	  pedIov->iov.insert(std::make_pair(iov_idx,pedtoken)); 
 	}	
       }
-    
-      svc->transaction().commit();
       std::cout << "iov size " << pedIov->iov.size() << std::endl;
-      svc->session().disconnectAll();
+      std::string pediovToken=iovwriter.markWrite<cond::IOV>(pedIov);
+      session->commit();//commit all in one
+      std::cout << "iov size " << pedIov->iov.size() << std::endl;
+      session->disconnect();
       std::cout << "committed" << std::endl;
-      std::cout << "iov tokens "<<iovtoken<< std::endl;
-      if( meta.addMapping("TrackerCalibration",iovtoken) ){
+      std::cout << "iov tokens "<<pediovToken<< std::endl;   
+      delete session;
+      if(metadata_svc.addMapping("TrackerCalibration", pediovToken)){
 	std::cout<<"new mapping inserted "<<std::endl;
       }else{
 	std::cout<<"mapping exists, do nothing "<<std::endl;
       }
+      metadata_svc.disconnect();
     }else {
-      std::cout<<"Reading "<<std::endl;
+      std::cout<<"Reading Mode"<<std::endl;
       std::string iovAName(cline[1]);
-      std::string iovAToken= meta.getToken(iovAName);
-      
+      std::string iovAToken= metadata_svc.getToken(iovAName);      
       std::cout << "iovAName " << iovAName << std::endl;
       std::cout << "iovAToken " << iovAToken << std::endl;
       
-      svc->transaction().start(pool::ITransaction::READ);
-      
+      session->connect(cond::ReadOnly);
+      std::cout<<"connected in ReadOnly mode to "<<dbConnection<<std::endl;
+      session->startReadOnlyTransaction();
+
+      //FIXME: Understand how to read db with new interface
+      /*
       pool::Ref<cond::IOV> iovA(svc,iovAToken);
-      //      std::pair<unsigned long,std::string> iovpair=*iovA->iov.lower_bound(0);
-      std::pair<unsigned long,std::string> iovpair=*iovA->iov.lower_bound(7);
-      std::cout<<"iov idx "<<iovpair.first<<std::endl;
-      std::string pedCid =iovpair.second;
-      std::cout << "ped Cid " << pedCid << std::endl;
+
+      cond::IOV* pedIov=new cond::IOV;
+      std::map<unsigned long,std::string>::iterator iov_iter=pedIov->iov.begin();
+      for(;iov_iter!=pedIov->iov.end();iov_iter++){
+
+	std::cout<<"iov idx "<<iov_iter->first<<std::endl;
+	std::string pedToken =iov_iter->second;
+	std::cout<<"reading Ped back by token "<<pedToken<<std::endl;
      
-      pool::Ref<SiStripPedestals> ped(svc,pedCid);
-
-      std::cout << "Pedestals for " << std::endl;
+	SiStripPedestals* ped=new SiStripPedestals();
 	
-      SiStripPedestalsMapIterator mapit = (*ped).m_pedestals.begin();
-      for (;mapit!=(*ped).m_pedestals.end();mapit++)
-	{
-	  unsigned int numschema = (*mapit).first;
-	    std::cout << "mappit " <<  numschema << std::endl;
+	std::cout << "Pedestals for " << std::endl;
+      
+	SiStripPedestalsMapIterator mapit = ped->m_pedestals.begin();
+	for (;mapit!=ped->m_pedestals.end();mapit++){
+      	  unsigned int numschema = (*mapit).first;
+	  std::cout << "mappit " <<  numschema << std::endl;
 	  
-	  std::vector<int> values = DecodeApvIdValues(numschema);
-	  SiStripPedestalsVector theSiStripVector =  (*mapit).second;
-	  //const SiStripPedestalsVector theSiStripVector =  (*ped).getSiStripPedestalsVector(numschema);
-	 
-	  std::cout << "SiStripData for " 
-		    << " FedId_   \t" << values[0]
-		    << " feFPGA_  \t" << values[1]
-		    << " strip_   \t" << values[2]
-		    << " ApvPair_ \t" << values[3]
-		    << " numschema \t"<< numschema << std::endl;
-
-	  int strip=0;
-	  for(SiStripPedestalsVectorIterator iter=theSiStripVector.begin(); iter!=theSiStripVector.end(); iter++){
-	    
+      	  std::vector<int> values = DecodeApvIdValues(numschema);
+      	  SiStripPedestalsVector theSiStripVector =  (*mapit).second;
+      	  //const SiStripPedestalsVector theSiStripVector =  (*ped).getSiStripPedestalsVector(numschema);
+	  
+      	  std::cout << "SiStripData for " 
+      		    << " FedId_   \t" << values[0]
+      		    << " feFPGA_  \t" << values[1]
+      		    << " strip_   \t" << values[2]
+      		    << " ApvPair_ \t" << values[3]
+      		    << " numschema \t"<< numschema << std::endl;
+	  
+      	  int strip=0;
+      	  for(SiStripPedestalsVectorIterator iter=theSiStripVector.begin(); iter!=theSiStripVector.end(); iter++){
 	    std::cout << " strip " << strip++ << " =\t"
-		      << iter->getPed()     << " \t" 
-		      << iter->getNoise()   << " \t" 
-		      << iter->getLowTh()   << " \t" 
-		      << iter->getHighTh()  << " \t" 
-		      << iter->getDisable()<< " \t" 
-		      << std::endl; 	    
-	  } 
-	}
-    }//end else
-    std::cout << "commit catalog" << std::endl;
-    cat->commit();
-    delete svc;
-  }   
-  catch ( seal::Exception& e ) {
-    std::cout << e.what() << std::endl;
-    return 1;
+      		      << iter->getPed()     << " \t" 
+      		      << iter->getNoise()   << " \t" 
+      		      << iter->getLowTh()   << " \t" 
+      		      << iter->getHighTh()  << " \t" 
+      		      << iter->getDisable()<< " \t" 
+      		      << std::endl; 	    
+      	  } 
+      	}
+	}*/
+    }//end else   
+  }catch(const cond::Exception& er){
+    std::cout<<er.what()<<std::endl;
+    delete loader;
+    exit(-1);
+  }catch(const seal::Exception& er){
+    std::cout<<er.what()<<std::endl;
+    delete loader;
+    exit(-1);
+  }catch(...){
+    std::cout<<"Funny error"<<std::endl;
+    delete loader;
+    exit(-1);
   }
-  catch ( std::exception& e ) {
-    std::cout << e.what() << std::endl;
-    return 1;
-  }
-  catch ( ... ) {
-    std::cout << "Funny error" << std::endl;
-    return 1;
-  }
-  
-  // timRep.dump();
-  
-  std::cout << "finished" << std::endl;
-  
+  delete loader;
+  std::cout << "finished" << std::endl;  
   return 0;
 };
 
@@ -245,13 +210,13 @@ int main(int csize, char** cline ) {
 
 unsigned int EncodeApvIdValues(int FedId_, int feFPGA_, int channel_, int ApvPair_)
 {
-  unsigned int apvpairid=0;
-  apvpairid = 
-    ( (( FedId_  - 1) & FedId_Mask_   ) << FedId_StartBit_   ) | 
-    ( (( feFPGA_ - 1) & feFPGA_Mask_  ) << feFPGA_StartBit_  ) | 
-    ( (( channel_- 1) & channel_Mask_ ) << channel_StartBit_ ) | 
-    ( (( ApvPair_- 1) & ApvPair_Mask_ ) << ApvPair_StartBit_ );
-  return apvpairid;
+unsigned int apvpairid=0;
+apvpairid = 
+( (( FedId_  - 1) & FedId_Mask_   ) << FedId_StartBit_   ) | 
+( (( feFPGA_ - 1) & feFPGA_Mask_  ) << feFPGA_StartBit_  ) | 
+( (( channel_- 1) & channel_Mask_ ) << channel_StartBit_ ) | 
+( (( ApvPair_- 1) & ApvPair_Mask_ ) << ApvPair_StartBit_ );
+return apvpairid;
 };
 
 // uint32_t EncodeStripData(float ped_, float noise_, float lowTh_, float highTh_, bool disable_)
