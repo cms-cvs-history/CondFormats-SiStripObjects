d76be5b13414eef644ca403acd86a5b820431c76
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-11-20 15:01:38 +0000

    removed test code

--- test/plugins/modules.cc
+++
@@ -1,5 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_SEAL_MODULE();
-
-#include "CondFormats/SiStripObjects/test/plugins/testSiStripFedCabling.h"
-DEFINE_ANOTHER_FWK_MODULE(testSiStripFedCabling);
