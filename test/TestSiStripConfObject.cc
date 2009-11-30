61f2be64d0a262134cfcf87f1743f124e71cb477
Marco De Mattia <sha1-48e734473f217d6ce6160a40224cbe0c67840e74@cern.ch>
2009-11-30 18:09:34 +0000

    Moved all test executables to test dir

--- test/TestSiStripConfObject.cc
+++ test/TestSiStripConfObject.cc
@@ -4,8 +4,19 @@
 
 using namespace std;
 
+int test( const bool testResult )
+{
+  if( testResult ) {
+    return 0;
+  }
+  cout << "test not passed" << endl;
+  return 1;
+}
+
 int main()
 {
+  int errors = 0;
+
   cout << "Testing SiStripConfObject" << endl;
 
   SiStripConfObject conf;
@@ -16,8 +27,20 @@ int main()
 
   cout << "Reading back parameters" << endl;
   cout << "getting par1 = " << conf.get("par1") << endl;
-  cout << "getting par2 = " << conf.get("par2") << endl;
+  errors += test( conf.get("par1") == 1 );
 
-  cout << "Trying to read back a non-existent parameter" << endl;
+  cout << "getting par2 = " << conf.get("par2") << endl;
+  errors += test( conf.get("par2") == 2 );
+
+  cout << "Trying to read back a non-existent parameter, the test expects an error" << endl;
   cout << "getting par3 (non-existent) " << conf.get("par3") << endl; 
+  errors += test( conf.get("par3") == -1 );
+
+  if( errors == 0 ) {
+    cout << "All tests passed" << endl;
+  }
+  else {
+    cout << "ERROR: There were " << errors << " tests not passed" << endl;
+  }
+
 }
